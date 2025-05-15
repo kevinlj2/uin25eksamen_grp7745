3e728b1e1c49c5de84371c70d8c0cb4c6026eab7
DWalle03 <didrikbunes@outlook.com>
2025-05-15 17:52:51 +0200

    Install/Logo

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,23 @@
 import { Link } from "react-router-dom";
 
 export default function Layout({ children }) {
+  const Logo = () => {
+    return (
+      <Link to="/">
+        <h1>BillettLyst</h1>
+      </Link>
+    );
+  };
+
   return (
     <>
       <header>
+        <Logo />
         <nav>
           <ul>
-            <Link to="category/music">Musikk</Link>
-            <Link to="category/sport">Sport</Link>
-            <Link to="category/theatre">Teater / Show</Link>
+            <Link to="category/music">Musikk </Link>
+            <Link to="category/sport">Sport </Link>
+            <Link to="category/theatre">Teater/Show </Link>
             <Link to="dashboard">Logg Inn</Link>
           </ul>
         </nav>
