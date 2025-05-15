4f751a1e1c649701c3ffe63e5acbdf782be3a940
kevinlj2 <kevlok4@gmail.com>
2025-05-15 22:05:30 +0200

    Oppdatert

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,13 @@ import Dashboard from "./components/Dashboard";
 function App() {
   const [festivals, setFestivals] = useState([]);
 
+  const FestivalEvents = [
+    "Z698xZb_Z17q339",
+    "Z698xZb_Z16v7eGkFy",
+    "Z698xZb_Z17qfaA",
+    "Z698xZb_Z17q3qg",
+  ];
+
   const getFestivals = async () => {
     fetch(
       "https://app.ticketmaster.com/discovery/v2/events.json?apikey=jj8DstPJ27elraSgsFdMAgqVpAMAGILh"
@@ -17,10 +24,12 @@ function App() {
       .then((data) => setFestivals(data.data))
       .catch((error) => console.error("Skjedde en feil ved fetch", error));
   };
+
   useEffect(() => {
     getFestivals();
     console.log("Min state", festivals);
   });
+
   return (
     <Layout>
       <Routes>
